7774a7438408dd8e986e0fb1d4ffc2840c0b9b95
x414e54 <x414e54@linux.com>
2015-08-12 21:20:56 +0900

    convert x3d matrix to bullet

--- x3dscene.cpp
+++ x3dscene.cpp
@@ -226,9 +226,9 @@ void X3DScene::update()
 
                 btCollisionShape* bt_collision = new btBoxShape(btVector3(size[0], size[1], size[2]));
                 btDefaultMotionState* bt_motionstate = new btDefaultMotionState(btTransform(
-                        btMatrix3x3(trans[0][1], trans[0][1], trans[0][1],
-                                    trans[1][1], trans[1][2], trans[1][3],
-                                    trans[2][1], trans[2][2], trans[2][3]),
+                        btMatrix3x3(trans[0][0], trans[0][1], trans[0][2],
+                                    trans[1][0], trans[1][1], trans[1][2],
+                                    trans[2][0], trans[2][1], trans[2][2]),
                         btVector3(trans[3][0], trans[3][1], trans[3][2])
                         ));
 
