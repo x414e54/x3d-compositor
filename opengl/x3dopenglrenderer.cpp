e4d57396d4edc5f5dad8ea2844cff1411385bcd6
x414e54 <x414e54@linux.com>
2015-08-30 16:18:58 +0900

    Move frame incrementing

--- opengl/x3dopenglrenderer.cpp
+++ opengl/x3dopenglrenderer.cpp
@@ -462,8 +462,6 @@ void X3DOpenGLRenderer::render(SceneGraph *sg)
     write_batches(get_draw_buffer(), get_draw_info_buffer());
 
     render_viewpoints();
-
-    ++frame_num %= StreamedBuffer::NUM_FRAMES;
 }
 
 bool X3DOpenGLRenderer::get_ray(Scalar x, Scalar y,
